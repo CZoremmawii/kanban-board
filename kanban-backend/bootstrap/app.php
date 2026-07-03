4d8e19b66d6dfcdfa2b58ad793220b37ee0fc388
CZoremmawii <cindyzorem@gmail.com>
2026-07-03 14:31:57 +0530

    your message

--- kanban-backend/bootstrap/app.php
+++ kanban-backend/bootstrap/app.php
@@ -21,4 +21,4 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
-    })->create();
+    })->create();   
--- kanban-backend/routes/web.php
+++ kanban-backend/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KanbanController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/columns', [KanbanController::class, 'getColumns']);
+Route::post('/cards', [KanbanController::class, 'addCard']);
+Route::delete('/cards/{id}', [KanbanController::class, 'deleteCard']);
+Route::patch('/cards/{id}/move', [KanbanController::class, 'moveCard']);
